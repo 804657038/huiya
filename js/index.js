30ae2150234c478c25951bf1503c1661962b2d72
cyh <385323977@qq.com>
2017-09-16 00:42:36 +0800

    91642

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var zero = document.getElementById('videozero');
+var one = document.getElementById('videoone');
+var two = document.getElementById('videotwo');
+var three = document.getElementById('videothree');
 var mySwiperHome = new Swiper('.swiper-container-homepage',{
 	loop : true,
 	autoplay: 5000,
@@ -24,8 +28,56 @@ var mySwiperSix = new Swiper('.swiper-container-pageSix1',{
 var mySwiperNine = new Swiper('.swiper-container-nine',{
 	direction: 'horizontal',
 	autoplayDisableOnInteraction : false,
+	pagination : '.swiper-pagination9',
 	slidesPerView: 'auto',
     centeredSlides: true,
     paginationClickable: true,
-    spaceBetween: 30   
+    spaceBetween: 30
+});
+$(function(){
+	$(document).on('touchstart','#lookmore',function(){
+		$('#page4').slideDown();
+	});
+	$(document).on('touchstart','#close',function(){
+		$('#page4').slideUp();		
+		two.pause();
+		$('#videotwo').hide();
+		$('#videotwo').siblings('.playPop').show();
+		three.pause();
+		$('#videothree').hide();
+		$('#videothree').siblings('.playPop').show();
+	});
+	$(document).on('touchstart','#playone',function(){
+		$('#videoone').show();
+		document.getElementById('videoone').play();
+	});
+	$(document).on('touchstart','#playone',function(){
+		$('#videoone').show();
+		document.getElementById('videoone').play();
+	});
+	
+	$(document).on('touchstart','#playzero',function(){
+		$('#videozero').siblings('.playPop').hide();
+		$('#videozero').show();
+		zero.play();
+	});
+	
+	$(document).on('touchstart','#playtwo',function(){
+		$('#videotwo').siblings('.playPop').hide();
+		$('#videotwo').show();
+		two.play();
+		three.pause();
+		$('#videothree').hide();
+		$('#videothree').siblings('.playPop').show();
+	});
+	
+	$(document).on('touchstart','#playthree',function(){
+		$('#videothree').siblings('.playPop').hide();
+		$('#videothree').show();
+		three.play();
+		two.pause();
+		$('#videotwo').hide();
+		$('#videotwo').siblings('.playPop').show();
+	});
+	
 });
