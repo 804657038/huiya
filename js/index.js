88bcc469b320e6d1a998cb490003fc9050a9b230
cyh <385323977@qq.com>
2017-09-15 20:16:55 +0800

    9152016

---
+++ js/index.js
@@ -0,0 +1,31 @@
+var mySwiperHome = new Swiper('.swiper-container-homepage',{
+	loop : true,
+	autoplay: 5000,
+	pagination : '.swiper-pagination',
+	direction: 'horizontal',
+	autoplayDisableOnInteraction : false
+});
+//5p
+var mySwiperFive = new Swiper('.swiper-container-five',{
+	loop : true,
+	autoplay: 5000,
+	pagination : '.swiper-pagination',
+	direction: 'horizontal',
+	autoplayDisableOnInteraction : false
+});
+//6p
+var mySwiperSix = new Swiper('.swiper-container-pageSix1',{
+	loop : true,
+	autoplay: 5000,
+	direction: 'horizontal',
+	autoplayDisableOnInteraction : false
+});
+//9p
+var mySwiperNine = new Swiper('.swiper-container-nine',{
+	direction: 'horizontal',
+	autoplayDisableOnInteraction : false,
+	slidesPerView: 'auto',
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 30   
+});
